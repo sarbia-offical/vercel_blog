f7d02e6b5cb4ee1955cf78ef7fa4f7bf0c3c7bcd
WwwanQin <xm36447864@126.com>
2023-06-15 11:35:57 +0800

    修改打包问题

--- component/Layout/index.tsx
+++ component/Layout/index.tsx
@@ -1,8 +1,17 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: zouwenqin
+ * @Date: 2023-06-15 10:59:14
+ * @LastEditors: zouwenqin
+ * @LastEditTime: 2023-06-15 11:35:25
+ */
 import type { NextPage } from 'next';
 import Navbar from 'component/Navbar';
 import Footer from 'component/Footer';
 import styles from './index.module.scss';
-const Layout: NextPage = (props) => {
+import React from "react"
+const Layout: any = (props: React.PropsWithChildren<{}>) => {
   return (
     <div>
       <Navbar></Navbar>
